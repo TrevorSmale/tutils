487df6c871dbd6c4abcb8d11822cf119226751d9
TrevorSmale <smaletrevor@gmail.com>
2021-10-08 20:56:51 -0400

    Initial Init Commit

---
+++ _init_.py
@@ -0,0 +1,26 @@
+# This program is distributed in the hope that it will be useful, but
+# Wihtout any Warranty; without even the impied warranty of 
+# Merchantibility or Fitness for a particular purpose. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# Along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+        "name" : "tutils",
+        "author" : "Trevor Smale",
+        "description" : "Utility Panel",
+        "blender" : (2, 80, 0),
+        "version" : (0, 0, 1,),
+        "location" : "view3d",
+        "warning" : "",
+        "category" : "Object"
+
+    }
+
+def register():
+    ...
+
+def unregister():
+    ...
+
